784bf431b16cc40a0cf0ff5eb9129a45d390841c
Jonathan <jonathan.m.official06@gmail.com>
2025-11-06 22:37:28 +0000

    menu update 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.getElementById('menu-btn')?.addEventListener('click', () => {
+  document.body.classList.toggle('menu-open');
+});
