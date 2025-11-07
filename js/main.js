1be7e93cd5f70dfdf1004883326f27f3a9391b1d
Jonathan <jonathan.m.official06@gmail.com>
2025-11-07 23:25:13 +0000

    fixed menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+const menu = document.querySelector('#mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('is-active');
+  navList.classList.toggle('active');
+});
