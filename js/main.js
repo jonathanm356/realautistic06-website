3b35b700977419f2a700ebb69487306b9aee1059
Jonathan <jonathan.m.official06@gmail.com>
2025-11-09 23:00:17 +0000

    new menu in testing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.querySelectorAll('.nav-list a').forEach(a =>
+  a.addEventListener('click', ()=> document.body.classList.remove('menu-open')));
